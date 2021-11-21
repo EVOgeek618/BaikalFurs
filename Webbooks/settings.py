dcd601c48b2b0b9a284cd7d9b689309bfd7658b2
EVOgeek618 <37446747+EVOgeek618@users.noreply.github.com>
2021-11-21 23:01:33 +0300

    Update settings.py

--- Webbooks/settings.py
+++ Webbooks/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'Webbooks.Webbooks.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'Webbooks/db.sqlite3',
+        'NAME': BASE_DIR / 'Webbooks.db.sqlite3',
     }
 }
 
