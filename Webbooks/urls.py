81d123863e538ef6a377c61eb48f778a2147bb62
EVOgeek618 <stevenorton2610@gmail.com>
2021-11-25 22:31:07 +0300

    finish

--- Webbooks/urls.py
+++ Webbooks/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
 from firsttry import views
+from django.conf.urls.static import static
 urlpatterns = [
     path('', views.home),
     path('faq/', views.faq),
@@ -30,3 +32,5 @@ urlpatterns = [
     path('otchet/<str:name>/', views.otchet),
     path('admin/', admin.site.urls),
 ]
+if settings.DEBUG: # new
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
